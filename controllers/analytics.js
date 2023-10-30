81a103e4c77949696d2426119d411af3f8c7315c
Emmanuel Okpunor <okpunorrex@gmail.com>
2023-10-30 22:33:54 +0100

    chore: analytics

--- controllers/analytics.js
+++ controllers/analytics.js
@@ -1,65 +1,82 @@
-const data = {
-  totalRevenue: 0,
-  totalRefunds: 0,
-  totalShops: 10,
-  todaysRevenue: 0,
-  totalOrders: 0,
-  newCustomers: 1,
-  totalYearSaleByMonth: [
-    {
-      total: 4,
-      month: "January",
-    },
-    {
-      total: 5,
-      month: "February",
-    },
-    {
-      total: 7,
-      month: "March",
-    },
-    {
-      total: 5,
-      month: "April",
-    },
-    {
-      total: 7,
-      month: "May",
-    },
-    {
-      total: 9,
-      month: "June",
-    },
-    {
-      total: 8,
-      month: "July",
-    },
-    {
-      total: 9,
-      month: "August",
-    },
-    {
-      total: 0,
-      month: "September",
-    },
-    {
-      total: 0,
-      month: "October",
-    },
-    {
-      total: 0,
-      month: "November",
-    },
-    {
-      total: 0,
-      month: "December",
-    },
-  ],
-}
+const Order = require("../models/Order")
+const {
+  subDays,
+  startOfToday,
+  startOfDay,
+  endOfDay,
+  format,
+  startOfYear,
+  endOfYear,
+} = require("date-fns")
+const Payment = require("../models/Payment")
 
-const getAnalytics = (req, res, next) => {
+const getAnalytics = async (req, res, next) => {
   try {
-    res.status(200).json({ success: true, data })
+    const yearStart = startOfYear(new Date())
+    const yearEnd = endOfYear(new Date())
+    const thirtyDaysAgo = subDays(startOfToday(), 30)
+    const todayStart = startOfDay(new Date())
+    const todayEnd = endOfDay(new Date())
+
+    const totalOrders = await Order.count({
+      createdAt: { $gte: thirtyDaysAgo },
+    })
+    const totalRevenue = await Payment.count({
+      createdAt: { $gte: thirtyDaysAgo },
+      status: "success",
+    })
+
+    const todaysOrders = await Order.count({
+      createdAt: { $gte: todayStart, $lt: todayEnd },
+    })
+    const todaysRevenue = await Payment.count({
+      createdAt: { $gte: todayStart, $lt: todayEnd },
+      status: "success",
+    })
+
+    const result = await Order.aggregate([
+      {
+        $match: {
+          createdAt: { $gte: yearStart, $lt: yearEnd },
+          status: "processing",
+        },
+      },
+      {
+        $group: {
+          _id: { $month: "$createdAt" },
+          totalAmount: { $sum: "$totalAmount" },
+        },
+      },
+    ])
+
+    let totalYearSaleByMonth = []
+
+    for (let i = 0; i < 12; i++) {
+      for (let j = 0; j < result.length; j++) {
+        const resultElement = result[j]
+        const month = resultElement._id
+        if (i === month) {
+          const monthName = format(new Date(2023, month - 1, 1), "MMMM")
+          const totalAmount = resultElement.totalAmount
+          totalYearSaleByMonth[i] = { total: totalAmount, month: monthName }
+          break // breaks out of the loop, so you don't overwrite totalYearSaleByMonth[i]
+        } else {
+          const monthName = format(new Date(2023, i, 1), "MMMM")
+          totalYearSaleByMonth[i] = { total: 0, month: monthName }
+        }
+      }
+    }
+
+    res.status(200).json({
+      success: true,
+      data: {
+        totalOrders,
+        todaysOrders,
+        totalRevenue,
+        todaysRevenue,
+        totalYearSaleByMonth,
+      },
+    })
   } catch (error) {
     next(error)
   }
